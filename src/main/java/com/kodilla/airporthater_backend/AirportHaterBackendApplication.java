a765bd582beec70968f36ec4e44dbb303ec8dd86
Zuzanna Jaworska <zuzanna.ewa.lewandowska@gmail.com>
2024-05-08 15:18:09 +0200

    First commit

---
+++ src/main/java/com/kodilla/airporthater_backend/AirportHaterBackendApplication.java
@@ -0,0 +1,13 @@
+package com.kodilla.airporthater_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirportHaterBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AirportHaterBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kodilla/airporthater_backend/AirportHaterBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodilla.airporthater_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirportHaterBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
